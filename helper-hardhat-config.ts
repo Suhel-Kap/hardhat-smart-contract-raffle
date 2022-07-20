2f861e1bcf9ed154c62cce4a5cb7a125739f6d2e
Suhel-Kap <suhelkapadia2@gmail.com>
2022-07-20 16:40:39 +0530

    Add frontend compatibility

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -18,7 +18,7 @@ export const networkConfig: networkConfigInfo = {
     4: {
         name: "rinkeby",
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        raffleEntranceFee: ethers.utils.parseEther("0.001").toString(),
+        raffleEntranceFee: ethers.utils.parseEther("0.1").toString(),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         subscriptionId: "8757",
         callbackGasLimit: "500000", // 500,000
@@ -26,7 +26,7 @@ export const networkConfig: networkConfigInfo = {
     },
     31337: {
         name: "hardhat",
-        raffleEntranceFee: ethers.utils.parseEther("0.001").toString(),
+        raffleEntranceFee: ethers.utils.parseEther("0.1").toString(),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         callbackGasLimit: "500000", // 500,000
         interval: "30",
